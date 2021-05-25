54a1d7afd24e41aee8f806fca3fcb4640deca2ee
Riswan Agyeman <agyeman.riswan@gmail.com>
2021-05-25 12:33:11 -0500

    Added a profile routes to display each player's data

--- routes/api.js
+++ routes/api.js
@@ -2,23 +2,27 @@
 const express = require('express')
 const router = express.Router()
 
-/*  This is a sample API route. */
+const mongoose = require('mongoose')
+const Profile = require('../models/Profile')
+mongoose.model('Profile');
 
-router.get('/:resource', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    query: req.query // from the url query string
-  })
+/*API route. */
+
+router.get('/profile', (req, res) => {
+  //Find: Movie.find({}).then(data => console.log(data))
+  Profile.find({}).then(data => console.log(data))
+
+    .catch(err => {
+      res.json({
+        confirmation: 'Fail',
+        data: err.message
+      })
+    })
+
+
+
 })
 
-router.get('/:resource/:id', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    id: req.params.id,
-    query: req.query // from the url query string
-  })
-})
+
 
 module.exports = router
